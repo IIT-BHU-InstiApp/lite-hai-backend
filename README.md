a8d647bb891a0eaef7fcc0510251e8ffe6f9a001
Ashish Kumar <ashishkr351@gmail.com>
2019-10-07 19:23:07 +0530

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-IIT BHU Workshops App
+# IIT (BHU) Workshops App Backend
+
+This repository hosts the source code for the backend server used by IIT (BHU) Workshops App.
+
+
+# Setting up Development Environment
+
+ - Install python dependencies using the command `pipenv install --ignore-pipfile` Please use only pipenv for managing dependencies(Follow this [link](https://realpython.com/pipenv-guide/) if you are new to pipenv).
+ - Run `python manage.py migrate` to apply migrations.
+ - Start the development server using `python manage.py runserver`
+ - To activate this project's virtualenv, run `pipenv shell`.
+ - Alternatively, run a command inside the virtualenv with `pipenv run`.
---
+++ workshops/urls.py
@@ -0,0 +1,38 @@
+"""workshops URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+admin.site.site_header='IIT (BHU) Workshops App Backend Administration'
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="IIT (BHU) Workshops App",
+      default_version='v1',
+      description="This is the schema for the IIT (BHU) Workshops App built using Django Rest Framework",
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+]
