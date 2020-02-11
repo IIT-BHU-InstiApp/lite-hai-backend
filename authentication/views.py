3f7c8b68bb77b7cf374fb5059275869221ee7b30
Ashish Kumar <ashishkr23438@gmail.com>
2020-02-11 16:11:26 +0530

    Modify login/ and profile/ endpoints

--- authentication/views.py
+++ authentication/views.py
@@ -6,14 +6,18 @@ from .serializers import *
 from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
 
+
 def create_auth_token(user):
     token, created = Token.objects.get_or_create(user=user)
     return token
 
 
 class LoginView(generics.GenericAPIView):
+    """
+    Checks the credentials and returns the REST Token (Authentication Token) if the credentials are valid.
+    """
     authentication_classes = []
-    permission_classes = (permissions.AllowAny,)
+    permission_classes = (permissions.AllowAny, )
     serializer_class = LoginSerializer
 
     def post(self,request):
@@ -21,12 +25,16 @@ class LoginView(generics.GenericAPIView):
         self.serializer.is_valid(raise_exception=True)
         self.user = self.serializer.validated_data['user']
         self.token = create_auth_token(self.user)
-        response = ResponseSerializer({'token':self.token})
-        return Response(response.data,status.HTTP_200_OK)
+        response = ResponseSerializer({'token': self.token})
+        return Response(response.data, status.HTTP_200_OK)
 
 
-class ProfileView(generics.RetrieveAPIView):
-    permission_classes = (permissions.IsAuthenticated,)
+class ProfileView(generics.RetrieveUpdateAPIView):
+    """
+    <b>GET</b>: Returns the Name, Email, Department and Year of Joining of the user.
+    <b>PUT/PATCH</b>: Updates the name of User Profile and returns the Name, Email, Department and Year of Joining.
+    """
+    permission_classes = (permissions.IsAuthenticated, )
     serializer_class = ProfileSerializer
 
     def get_object(self):
