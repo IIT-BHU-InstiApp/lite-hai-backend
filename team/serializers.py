3f37f1074af3d580eed2929a82545080bd43aced
Ashish Kumar <ashishkr351@gmail.com>
2020-04-17 16:39:36 +0530

    Modify team model - update github image on save (#19)

--- team/serializers.py
+++ team/serializers.py
@@ -1,28 +1,7 @@
 from rest_framework import serializers
-import requests
 from .models import TeamMember, Role
 
-def get_github_profile_pic_url(username):
-    """
-    Get the profile picture url from the github handle
-    """
-    r = requests.get('https://api.github.com/users/' + username)
-    github_user_json = r.json()
-    return github_user_json['avatar_url']
-
 class TeamMemberSerializer(serializers.ModelSerializer):
-    github_image_url = serializers.SerializerMethodField()
-
-    def get_github_image_url(self, obj):
-        """
-        Get the value of github_image_url
-        """
-        if not obj.github_image_url:
-            obj.github_image_url = get_github_profile_pic_url(obj.github_username)
-            obj.save()
-
-        return obj.github_image_url
-
     class Meta:
         model = TeamMember
         fields = ('name', 'github_username', 'github_image_url')
