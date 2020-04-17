52465e337dfb3503b02c1ae090d04028e3f7a952
Ashish Kumar <ashishkr23438@gmail.com>
2020-04-17 16:12:09 +0530

    Modify team model - update github image on save

--- team/models.py
+++ team/models.py
@@ -1,4 +1,13 @@
 from django.db import models
+import requests
+
+def get_github_profile_pic_url(username):
+    """
+    Get the profile picture url from the github handle
+    """
+    r = requests.get('https://api.github.com/users/' + username)
+    github_user_json = r.json()
+    return github_user_json['avatar_url']
 
 class Role(models.Model):
     role = models.CharField(max_length=100)
@@ -13,6 +22,11 @@ class TeamMember(models.Model):
                              blank=True, related_name='team_members')
     github_username = models.CharField(max_length=50)
     github_image_url = models.URLField(null=True, blank=True, editable=False)
+
+    # pylint: disable=arguments-differ
+    def save(self, *args, **kwargs):
+        self.github_image_url = get_github_profile_pic_url(self.github_username)
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.name
