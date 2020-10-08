dfb84d382381a032bc33aeb1f091c93f30cc75d3
David <davidgarg28@gmail.com>
2020-10-08 20:11:29 +0530

    FCM Authorized

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,14 +1,14 @@
 from django.urls import path
 from django.conf.urls import url, include
 from .views import LoginView, ProfileView, ProfileSearchView
-from fcm_django.api.rest_framework import FCMDeviceViewSet
+from fcm_django.api.rest_framework import FCMDeviceAuthorizedViewSet
 
 from rest_framework.routers import DefaultRouter
 
 
 router = DefaultRouter()
 
-router.register(r'devices', FCMDeviceViewSet)
+router.register(r'devices', FCMDeviceAuthorizedViewSet)
 
 urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
