12e7ee0f8b222037377406f8f7167710e2a4ff69
Ashish Kumar <ashishkr23438@gmail.com>
2020-10-14 10:14:08 +0530

    Change url to path

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from django.conf.urls import url, include
+from django.conf.urls import include
 from rest_framework.routers import DefaultRouter
 from fcm_django.api.rest_framework import FCMDeviceAuthorizedViewSet
 from .views import LoginView, ProfileView, ProfileSearchView
@@ -12,5 +12,5 @@ urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
     path('profile/', ProfileView.as_view(), name='profile'),
     path('profile/search/', ProfileSearchView.as_view(), name='profile-search'),
-    url(r'^', include(router.urls)),
+    path('', include(router.urls)),
 ]
