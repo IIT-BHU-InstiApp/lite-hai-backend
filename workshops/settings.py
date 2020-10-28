6c0248749d308f9035fe1fec0447fa242986eafe
Nishtha Bodani <nbodani8@gmail.com>
2020-10-28 21:24:58 +0530

    Modified log settings
    Co-authored-by: Ashish Kumar <ashishkr23438@gmail.com>

--- workshops/settings.py
+++ workshops/settings.py
@@ -191,6 +191,7 @@ LOGGING = {
         'django': {
             'handlers': ['console'],
             'level': os.getenv('DJANGO_LOG_LEVEL', 'INFO'),
+            'propagate': False,
         },
     },
 }
