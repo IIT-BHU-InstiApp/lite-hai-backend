8d714c3ebc0e211d6488711fabc4864eeb8f3a84
Lakshya Singh <lakshay.singh1108@gmail.com>
2020-12-09 17:13:31 +0530

    Decrease amount of times db is hit
    using select_related and prefetch_related mke sql joins
    rather than hitting database again to fetch those values

--- workshop/views.py
+++ workshop/views.py
@@ -35,7 +35,7 @@ class ClubDetailView(generics.RetrieveUpdateAPIView):
     Update the description of a Club (Partial Update).
     """
     # pylint: disable=no-member
-    queryset = Club.objects.all()
+    queryset = Club.objects.select_related('joint_secy','secy').all()
     permission_classes = (AllowParticularClubHead,)
     serializer_class = ClubDetailSerializer
 
@@ -206,7 +206,7 @@ class CouncilDetailView(generics.RetrieveUpdateAPIView):
     Update the description of a Council (Partial Update).
     """
     # pylint: disable=no-member
-    queryset = Council.objects.all()
+    queryset = Council.objects.select_related('gensec','joint_gensec').prefetch_related('clubs').all()
     permission_classes = (AllowParticularCouncilHead,)
     serializer_class = CouncilDetailSerializer
 
