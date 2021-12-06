ce9ca0cdaf568b8dfe210ddb48fbeb06ec8da7ae
ankur12-1610 <anknerd.12@gmail.com>
2021-12-06 18:00:12 +0530

    Created noticeboard app

---
+++ noticeboard/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class NoticeBoard(models.Model):
+    name = query = models.CharField(max_length=50)
+    description = models.TextField(null=True, blank=True)
+    date = models.DateTimeField()
+    ping = models.BooleanField(default=False)
+    link = models.URLField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name + ' - ' + self.description
--- workshops/settings.py
+++ workshops/settings.py
@@ -53,6 +53,7 @@ INSTALLED_APPS = [
     'authentication',
     'workshop',
     'team',
+    'noticeboard',
     'config'
 ]
 
