cf54df6dc591f05a93afe51dccc24b81c16d8c0b
Somesh Ranjan <77766761+somesh202@users.noreply.github.com>
2021-12-09 23:32:50 +0530

    Modified NoticeBoard Serializers (#2)
    * small fixes
    
    * added get and create serializers for notice

--- noticeboard/models.py
+++ noticeboard/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 
 class NoticeBoard(models.Model):
-    name = models.CharField(max_length=50)
+    title = models.CharField(max_length=50)
     description = models.TextField(null=True, blank=True)
     date = models.DateTimeField()
-    ping = models.BooleanField(default=False)
-    link = models.URLField(null=True, blank=True)
+    pin = models.BooleanField(default=False)
+    notice_url = models.URLField(null=True, blank=True)
     upvote = models.IntegerField(default=0)
     downvote = models.IntegerField(default=0)
     
     def __str__(self):
-        return self.name + ' - ' + self.description
+        return self.title + ' - ' + self.description
--- noticeboard/serializers.py
+++ noticeboard/serializers.py
@@ -1,9 +1,40 @@
 from datetime import date
 from rest_framework import serializers
-from .models import NoticeBoard
+from rest_framework.exceptions import PermissionDenied
+from .models import NoticeBoard, UserProfile
 
 
-class NoticeBoardSerializer(serializers.ModelSerializer):
+class NoticeGetSerializer(serializers.ModelSerializer):
     class Meta:
         model = NoticeBoard
-        fields = ('name', 'description', 'date', 'link', 'ping', 'upvote', 'downvote')
+        fields = '__all__'
+
+ 
+class NoticeCreateSerializer(serializers.ModelSerializer):
+    def validate(self, attrs):
+        request = self.context['request']
+        # pylint: disable=no-member
+        profile = UserProfile.objects.get(user=request.user)
+        print(profile.get_workshop_privileges())
+        if profile.get_workshop_privileges():
+            raise PermissionDenied(
+                "You are not authorized to create notices")
+        return attrs
+
+    def save(self, **kwargs):
+        data = self.validated_data
+        # pylint: disable=no-member
+        notice = NoticeBoard.objects.create(
+            title=data['title'], 
+            description=data.get('description', ''),
+            date=data.get('date', None),
+            notice_url=data.get('link', None)
+        )
+        # FirebaseAPI.send_entity_message(data, self.context['notices'])
+        return notice
+
+    class Meta:
+        model = NoticeBoard
+        fields = (
+             'title', 'description', 'date', 'notice_url'
+            )
