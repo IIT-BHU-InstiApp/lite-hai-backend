923c992670b46b8c4a8bb599bb73a29178b3e046
soumyo123-prog <deysoumyajyoti2017@gmail.com>
2022-02-06 12:58:32 +0530

    nit

--- mess/models.py
+++ mess/models.py
@@ -16,15 +16,22 @@ class Mess(models.Model):
     menu = models.URLField(max_length=200)
     hostel = models.ForeignKey(Hostel, on_delete=models.CASCADE)
 
+    class Meta:
+        verbose_name_plural = "messes"
+
     def __str__(self):
         return self.name
 
 
 class Bill(models.Model):
+    MONTH = ((1, 'January'), (2, 'February'), (3, 'March'), (4, 'April'), (5, 'May'), (6, 'June'),
+             (7, 'July'), (8, 'August'), (9, 'September'), (10, 'October'), (11, 'November'), (12, 'December'))
+
     user_profile = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
     mess = models.ForeignKey(Mess, on_delete=models.CASCADE)
     monthly_bill = models.IntegerField()
     extra_charges = models.IntegerField(default=0)
+    month = models.IntegerField(choices=MONTH)
 
     def __str__(self):
         return '{} {}'.format(self.user_profile.name, self.mess.name)
