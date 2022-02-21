16e2a1a13343334cdb84ff553bdea90d0544978c
Vikhyath Venkatraman <55887656+Vikhyath08@users.noreply.github.com>
2022-02-21 16:27:56 +0530

    Revert "Completed the mess functionality (#117)"
    This reverts commit 02063bcd4108da5c419e6eab434f8ec4f31c6c0a.

--- mess/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from . models import Mess, Hostel, Bill
-
-admin.site.register(Mess)
-admin.site.register(Hostel)
-admin.site.register(Bill)
--- mess/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class MessConfig(AppConfig):
-    name = 'mess'
